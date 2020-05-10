7a806040065ab155cda440b2550b694d08d5a690
Michael <michaelpsy71095@gmail.com>
2020-05-11 02:19:00 +0800

    Adding Tasks: Validation

--- task-list-app/app/Http/Controllers/TaskController.php
+++ task-list-app/app/Http/Controllers/TaskController.php
@@ -26,4 +26,19 @@ class TaskController extends Controller
     {
         return view('tasks.index');
     }
+
+    /**
+     * Create a new task.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        // Create The Task
+    }
 }
--- task-list-app/resources/views/tasks/index.blade.php
+++ task-list-app/resources/views/tasks/index.blade.php
@@ -6,6 +6,7 @@
 
     <div class="panel-body">
         <!-- Display Validation Errors -->
+        @include('common.errors')
 
         <!-- New Task Form -->
         <form action="{{ url('task') }}" method="POST" class="form-horizontal">
